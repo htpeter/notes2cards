0cccee2c0c06f450a3229bab77a098f8419a0549
peter <kouvaris.peter@gmail.com>
2018-10-30 21:33:08 -0400

    move stuff to the main file

--- notes2cards.py
+++ notes2cards.py
@@ -24,7 +24,61 @@ def main(args):
         # Z
         print(list_of_dicts)
 
+    my_deck.add_model()
 
+def note_parse(raw_text):
+	"""
+	WHERE THE MAGIC HAPPENS!
+	Turns notes2cards formatted notes into genanki cards.
+	Also stores them in a format for use later if we wanna
+	get fancy.
+	"""
+	list_of_dicts = raw_text
+	return list_of_sections
+
+def notify(dictionary, model_type):
+	"""
+	Turn a dictionary to a note based on a style
+	"""
+	# types of models for note
+	types = {
+		# Simple card with Question and Answer
+		'standard_model' : genanki.Model(
+				model_id = random.getrandbits(32),
+				name = 'standard_model',
+				fields=[
+				    {'name': 'Question'},
+				    {'name': 'Answer'},
+				  ],
+				templates=[
+				    {
+				      'name': 'Card 1',
+				      'qfmt': '{{Question}}',
+				      'afmt': '{{FrontSide}}<hr id="answer">{{Answer}}',
+				    },
+				  ]),
+		# Placeholder for more robust methods.
+		'fancy_model' : genanki.Model(
+				model_id = random.getrandbits(32),
+				name = 'fancy_model',
+			  fields=[
+				    {'name': 'Question'},
+				    {'name': 'Answer'},
+				  ],
+			  templates=[
+				    {
+				      'name': 'Card 1',
+				      'qfmt': '{{Question}}',
+				      'afmt': '{{FrontSide}}<hr id="answer">{{Answer}}',
+				    },
+				  ]),
+			}
+	# Generate note
+	note = genanki.Note( 
+		model = types[model_type],
+ 		fields=['Capital of Argentina', 'Buenos Aires'])
+	return note
+	
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(description='Turn some notes into a Anki deck.')
