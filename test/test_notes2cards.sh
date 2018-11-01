aa7b7ed361b753d7fef279dd2ad77ac19bcf2814
peter <kouvaris.peter@gmail.com>
2018-11-01 16:57:57 -0400

    Update test_notes2cards.sh

--- test/test_notes2cards.sh
+++ test/test_notes2cards.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 # executed by travis from ../
-python3.6 notes2cards.py --inputFile test_file.md \
-						--deckName Test_Deck --outputFile test_deck.apkg
+python3.6 notes2cards.py --inputFile ./test/test_file.md \
+						--deckName Test_Deck --outputFile ./test/test_deck.apkg
